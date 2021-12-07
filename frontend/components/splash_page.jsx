21e6b7148b91cdf45928e42b0d25a9cfddda49d4
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-12-07 14:21:13 -0500

    Splash page

--- frontend/components/splash_page.jsx
+++ frontend/components/splash_page.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Splash from "./splash_page/splash";
+import { Route } from "react-router";
 
 const SplashPages = () => {
     <div>
-        
+        <Route exact path="/" component={Splash}/>
     </div>
 }
 
---
+++ frontend/components/splash_page/splash.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+const Splash = () => {
+    return (
+        <div>
+            Welcome, Splash Page
+        </div>
+    )
+}
+
+export default Splash;
