7152a259ad00ab1595e668f051c72d0ffdc8a250
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-12-07 13:43:32 -0500

    Frontend Components & Store

--- frontend/masa_rentals.jsx
+++ frontend/masa_rentals.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import configureStore from "./store/store";
+
+
 document.addEventListener("DOMContentLoaded", () => {
+    let store = configureStore();
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to Masa Rentals!</h1>, root)
 })
